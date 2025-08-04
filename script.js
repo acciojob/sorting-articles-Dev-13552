87eeeb34343f6eb08670591ea812b41a132b18c0
Dev-13552 <devrathour110@gmail.com>
2025-08-04 20:44:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const ul = document.createElement("ul")
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+function stripArticle(name) {
+  return name.replace(/^(a |an |the )/i, '').trim();
+}
+
+bands.sort((a, b) => {
+  return stripArticle(a).localeCompare(stripArticle(b));
+});
+
+console.log(bands);
+bands.forEach((ele)=>{
+	const li = document.createElement('li')
+	li.textContent = ele
+	ul.append(li)
+})
+console.log(ul)
+document.body.append(ul)
