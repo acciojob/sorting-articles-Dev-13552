6ec200f6fd8c2e129b8f7022feaf4d62232aa6cd
Dev-13552 <devrathour110@gmail.com>
2025-08-04 20:55:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //your JS code here. If required.
-const ul = document.createElement("ul");
-ul.id = "band";
+const ul = document.querySelector("#band")
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 function stripArticle(name) {
   return name.replace(/^(a |an |the )/i, '').trim();
